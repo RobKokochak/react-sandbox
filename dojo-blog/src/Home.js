6d83653d101b2cf97eb3e9c88d5aff08e861b7ac
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2022-11-16 12:09:46 -0500

    useEffect and useEffect dependencies

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,15 +8,27 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  // useEffect fires the callback function in 1st argument when the page loads
+  // if no 2nd argument specified, useEffect fires on every re-render of the dom - whenever anything changes
+  // putting an empty array for 2nd argument means the dependency list is empty - meaning useEffect will only run on page load
+  // putting dependencies in the list means useEffect runs at page load and also  when those dependencies changes
+  useEffect(() => {
+    console.log('useEffect ran');
+    console.log(name);
+  }, [name]);
+
   return (  
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" handleDelete={handleDelete}/>
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
