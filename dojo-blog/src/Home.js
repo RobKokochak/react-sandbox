2035ada74bfc050049c756e84ba297cf4302b5ed
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2022-11-16 11:04:33 -0500

    Output dynamic lists using .map

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
   
-  const [name, setName] = useState('rob'); //the useState hook returns two things, a value and a function which allows access to change it
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
-
   return (  
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => ( // .map fires a callback function for each blog in blogs
+      // key must be used so React can distinguish between root elements in the dom
+        <div className="blog-preview" key={blog.id}> 
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
