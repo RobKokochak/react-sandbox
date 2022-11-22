fa7ddeef38419f779b830970762481519b40eb96
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2022-11-22 10:55:06 -0500

    handling fetch errors and showing error to client

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // useEffect fires the callback function in 1st argument when the page loads
   // if no 2nd argument specified, useEffect fires on every re-render of the dom - whenever anything changes
@@ -12,21 +13,31 @@ const Home = () => {
   useEffect(() => {
     // setTimeout is used to simulate real world request
     setTimeout(()=> {
-      fetch('http://localhost:8000/blogs')
+      fetch('http://localhost:8000/blogss')
       .then(res => {
+        console.log(res);
+        if(!res.ok) {
+          throw Error('could not fetch the data for that resource');
+        }
         return res.json();
       })
       .then(data => {
         setBlogs(data);
         setIsPending(false);
-      });
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
     }, 1000);
   }, []);
 
   return (  
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
     </div>
   );
 }
