1b0f1cee93304ce81ead85d851f1a1b7453a52f9
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2022-11-25 18:28:13 -0500

    setting up router to allow multiple pages

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,15 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  
   return (
-    <div className="App">
-    <Navbar />
-      <div className="content">
-        <Home />
+    // Router surrounds the entire app
+    <Router> 
+      <div className="App">
+      <Navbar />
+        <div className="content">
+        {/* anything outside of the switch component will not change - so the Navbar will always be there no matter which page you're on */}
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
